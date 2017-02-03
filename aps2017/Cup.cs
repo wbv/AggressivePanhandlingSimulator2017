3daefb1643d33c78fca60694d78a9423a509f355
Walter B Vaughan <walter.b.vaughan@gmail.com>
2017-02-03 16:25:46 -0600

    added animations, max high score

---
+++ aps2017/Cup.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Controls;
+using System.Windows.Media.Animation;
+
+namespace AggressivePanhandlingSimulator2017
+{
+	public class Cup
+	{
+		public Button Button { get; set; }
+		public Storyboard Storyboard { get; set; }
+
+		public Cup(Button button)
+		{
+			Button = button;
+		}
+	}
+}
--- aps2017/MainWindow.xaml.cs
+++ aps2017/MainWindow.xaml.cs
@@ -3,6 +3,7 @@ using System.Threading;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Threading;
 
 namespace AggressivePanhandlingSimulator2017
@@ -11,7 +12,7 @@ namespace AggressivePanhandlingSimulator2017
 	{
 		private Random rng;
 		private int winningCup;
-		private Button[] cups;
+		private Cup[] cups;
 		private uint streak;
 		private uint maxStreak;
 
@@ -26,11 +27,13 @@ namespace AggressivePanhandlingSimulator2017
 		public volatile bool gameTransition;
         private delegate void gameThreadUpdateOneArg(bool arg);
         private delegate void gameThreadUpdateNoArg();
-
+	
         private const string GamePrompt = "Pick a cup.";
 		private const string GamePromptAfterWin = "Push your luck.";
 		private const string GameWinPrompt = "Nice.";
 		private const string GameLossPrompt = "Bummer.";
+
+		private const uint MaxWins = 4;
 
 		public MainWindow()
 		{
@@ -45,7 +48,25 @@ namespace AggressivePanhandlingSimulator2017
 			CupDefaultColor    = new SolidColorBrush((Color)FindResource("CupDefaultColor"));
 
 			rng = new Random();
-			cups = new Button[] { Cup1, Cup2, Cup3 };
+			cups = new Cup[]
+			{
+				new Cup(Cup1), new Cup(Cup2), new Cup(Cup3)
+			};
+
+			var marginProperty = DependencyProperty.Register("Margin", typeof(Thickness), typeof(Button));
+			foreach (var cup in cups)
+			{
+				var animationFrames = new ThicknessKeyFrameCollection();
+				animationFrames.Add(new SplineThicknessKeyFrame(new Thickness(32, 20, 32, 108), KeyTime.FromTimeSpan(TimeSpan.FromSeconds(0.2))));
+				animationFrames.Add(new SplineThicknessKeyFrame(new Thickness(32, 20, 32, 108), KeyTime.FromTimeSpan(TimeSpan.FromSeconds(1.8))));
+				animationFrames.Add(new SplineThicknessKeyFrame(new Thickness(32, 64, 32, 64), KeyTime.FromTimeSpan(TimeSpan.FromSeconds(2))));
+				var animation = new ThicknessAnimationUsingKeyFrames();
+				animation.KeyFrames = animationFrames;
+				cup.Storyboard = new Storyboard();
+				cup.Storyboard.Children.Add(animation);
+				Storyboard.SetTargetName(animation, cup.Button.Name);
+				Storyboard.SetTargetProperty(animation, new PropertyPath(Button.MarginProperty));
+			}
 
 			gameTransition = false;
 			streak = 0;
@@ -54,60 +75,70 @@ namespace AggressivePanhandlingSimulator2017
 
 		public void NewGame(bool wonlasttime)
 		{
-			InfoLabel.Text = wonlasttime ? GamePromptAfterWin : GamePrompt;
-
-			for (int i = 0; i < cups.Length; i++)
+			if (streak >= MaxWins)
 			{
-				cups[i].Background = CupDefaultColor;
-				cups[i].Foreground = AppForegroundColor;
-				cups[i].BorderBrush = AppForegroundColor;
+				InfoLabel.Text = "Cheater.";
+				RemoveCups();
 			}
+			else
+			{ 
+				InfoLabel.Text = wonlasttime ? GamePromptAfterWin : GamePrompt;
+				for (int i = 0; i < cups.Length; i++)
+				{
+					cups[i].Button.Background = CupDefaultColor;
+					cups[i].Button.Foreground = AppForegroundColor;
+					cups[i].Button.BorderBrush = AppForegroundColor;
+				}
 
-			winningCup = rng.Next(3);
+				winningCup = rng.Next(3);
+			}
 		}
-		
+
 		private void Cup1_Clicked(object sender, RoutedEventArgs e)
 		{
-			Play(winningCup == 0);
+			Play(0);
 		}
 
 		private void Cup2_Clicked(object sender, RoutedEventArgs e)
 		{
-			Play(winningCup == 1);
+			Play(1);
 		}
 
 		private void Cup3_Clicked(object sender, RoutedEventArgs e)
 		{
-			Play(winningCup == 2);
+			Play(2);
 		}
 
-		private void Play(bool win)
+		private void Play(uint cup)
 		{
+			bool win = cup == winningCup;
+			
 			if (gameTransition) // ignore buttons if game has just been played
 				return;
 
-			// UI updates to show the play
-			RevealCups();
-			InfoLabel.Text = (win ? GameWinPrompt : GameLossPrompt);
+			cups[cup].Storyboard.Begin(this);
+
 			if (win)
 				streak++;
 			else
 				streak = 0;
+			
+			RevealCups();
+			InfoLabel.Text = (win ? GameWinPrompt : GameLossPrompt);
 
 			maxStreak = Math.Max(streak, maxStreak);
-
 			StreakLabel.Text = "Win Streak: " + streak;
 			BestStreakLabel.Text = "Best: " + maxStreak;
 
 			gameTransition = true;
-
-            // force a render of the UI
-            gameThreadUpdateNoArg g = () => { };
+			
+			// force a render of the UI
+			gameThreadUpdateNoArg g = () => { };
 			Application.Current.Dispatcher.Invoke( DispatcherPriority.Render, g);
-
+			
 			// start a task which will reset the UI after 2 seconds
 			Thread newthread = new Thread(new ParameterizedThreadStart(GameplayTimedThread));
-            newthread.Start();
+			newthread.Start();
 		}
 
 		private void RevealCups()
@@ -115,24 +146,32 @@ namespace AggressivePanhandlingSimulator2017
 			for (int i = 0; i < cups.Length; i++)
 			{
 				if (i == winningCup)
-				{ 
-					cups[i].Background = CupWinColor;
-					cups[i].Foreground = CupWinDarkColor;
-					cups[i].BorderBrush = CupWinDarkColor;
+				{
+					cups[i].Button.Background = CupWinColor;
+					cups[i].Button.Foreground = CupWinDarkColor;
+					cups[i].Button.BorderBrush = CupWinDarkColor;
 				}
 				else
 				{
-					cups[i].Background = CupLoseColor;
-					cups[i].Foreground = CupLoseDarkColor;
-					cups[i].BorderBrush = CupLoseDarkColor;
+					cups[i].Button.Background = CupLoseColor;
+					cups[i].Button.Foreground = CupLoseDarkColor;
+					cups[i].Button.BorderBrush = CupLoseDarkColor;
 				}
 			}
+		}
+
+		private void RemoveCups()
+		{
+			foreach (var cup in cups)
+				cup.Button.Visibility = Visibility.Hidden;
+			StreakLabel.Visibility = Visibility.Hidden;
+			BestStreakLabel.Visibility = Visibility.Hidden;
 		}
 
         private void GameplayTimedThread(object win)
         {
             gameThreadUpdateOneArg g = NewGame;
-            Thread.Sleep(TimeSpan.FromSeconds(2));
+            Thread.Sleep(TimeSpan.FromSeconds((streak >= MaxWins ? 1 : 2)));
             gameTransition = false;
             try
             { 
